e6005b233a538d2dfa67fcc4af3eba76c9cd4604
Marc <wm2041@mci4me.at>
2026-06-03 08:25:48 +0200

    misc fixes

--- advanced_powercurve.py
+++ advanced_powercurve.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     df_test = read_data("data/activities/activity.csv")
     df_test = add_time(df_test)
     #print(df_test.head())
-    #window_list = [10, 20, 30, 60, 300, 600, 1200, 1800, 3600, 7200]
+    #window_list = [10, 20, 30, 60, 300, 600, 1200, 1800, 3600, 7200] #original
     #window_list = [10, 20, 30, 40, 60, 90, 120, 240, 300, 400, 450, 500, 550, 600, 650, 700, 800, 900, 1000, 1200]
     window_list = create_windows(df_test)
     pcdf2 = create_pc_df(df_test, window_list)
@@ -60,4 +60,3 @@ if __name__ == "__main__":
     yaxis_title="Max. avg. Power in W"
     )
     fig1.show()
-    #fig1.write_image("/data/activities/advanced_power_curve.png")
